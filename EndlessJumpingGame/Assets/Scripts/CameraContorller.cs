e4f533498858e7a263aed428988c5b087f885e34
Kamil <rewwernick@gmail.com>
2020-02-29 13:46:54 +0300

    commit

---
+++ EndlessJumpingGame/Assets/Scripts/CameraContorller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContorller : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset; 
+
+    private void Start()
+    {
+        transform.LookAt(player.transform);
+        offset = transform.position - player.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position += (- transform.position + player.transform.position + offset) * Time.deltaTime * 4;
+    }
+}
--- EndlessJumpingGame/Assets/Scripts/PlayerController.cs
+++ EndlessJumpingGame/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,51 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed, horizontalSpeed, jumpForce, gravityScale;
+    private CharacterController characterContoroller;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    public GameObject platformPrefab;
+
+
+    private void Start()
+    {
+        characterContoroller = GetComponent<CharacterController>(); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            moveDirection = Vector3.zero;
+            characterContoroller.Move(Vector3.up * 1.6f - transform.position);
+            return; 
+        }
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * Mathf.Sqrt(2) * horizontalSpeed, moveDirection.y, Input.GetAxis("Horizontal") * Mathf.Sqrt(2) * horizontalSpeed);
+        moveDirection += (Vector3.forward + Vector3.left) * Mathf.Sqrt(2) * speed;
+
+        if (characterContoroller.isGrounded)
+        {
+            //if (Input.GetButtonDown("Jump"))
+            //{
+                moveDirection.y = jumpForce;
+            //}
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale); 
+
+        characterContoroller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void CreatePlatform() // then it will set skin for platforms 
+    {
+        float landingTime = jumpForce * jumpForce / (2 * Physics.gravity.y * gravityScale);
+
+        float defTime = Random.Range(landingTime / 2, landingTime);
+    }
 }
